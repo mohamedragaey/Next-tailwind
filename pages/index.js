319c8062e4a33b07ea6183d301f5e4a5fcfff64c
mohamedragaey <mohamedragaey33@gmail.com>
2021-03-07 16:27:50 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <main className='bg-white container m-auto mt-24 p-6 shadow'>
+        <h1 className='border-b border-red-500 mb-6 pb-3 text-2xl text-center text-red-900 transition uppercase'>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <p>
+          Get started by editing{' '}
+          <code>pages/index.js</code>
+        </p>
+
+        <div>
+          <a href="https://nextjs.org/docs">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a href="https://github.com/vercel/next.js/tree/master/examples">
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a href="https://vercel.com/new">
+            <h3>Deploy &rarr;</h3>
+            <p> Instantly deploy your Next.js site to a public URL with Vercel. </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  )
+}
